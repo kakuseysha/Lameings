223eea687784ae17758fb6d236e01cae92c2ba82
Bruno Furquim <kakuseysha@hotmail.com>
2016-05-18 20:09:02 -0300

    git ignore

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public enum TileTypes
+    {
+        Walkable,
+        Damaging,
+        Blocking,
+        Interactive
+    }
+
+    public TileTypes tileType = TileTypes.Walkable;
+
+}
